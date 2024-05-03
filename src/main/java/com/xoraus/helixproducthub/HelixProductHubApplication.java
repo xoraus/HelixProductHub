56dea327467dd49b63ae6dfd786f90cdce5f87ac
Sajjad Salaria <imsafay@gmail.com>
2024-05-03 09:55:30 +0530

    Initial Commit

---
+++ src/main/java/com/xoraus/helixproducthub/HelixProductHubApplication.java
@@ -0,0 +1,13 @@
+package com.xoraus.helixproducthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelixProductHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelixProductHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xoraus/helixproducthub/HelixProductHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xoraus.helixproducthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelixProductHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
